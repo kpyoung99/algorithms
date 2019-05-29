51b2334221da6dea040eadfdcc4cecad6cee140e
Kaid <kaitliny12@gmail.com>
2019-05-28 22:54:50 -0400

    kat04

--- kat04.cpp
+++ kat04.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-int main(){
-    int num, temps, numDays;
-    std::cin >> num;
-    std::cin >> temps;
-    numDays = days();
-    std::cout << numDays << std::endl;
-    return 0;
-}
 
-int days(int num, int[] ){
-    int days = 0;
-    for (int i = 0; i < num; i++){
-        if(temp < 0)    days++;
+int main(){
+    int n;
+    std::cin >> n;
+    int temps[n];
+    int frz = 0;
+    for (int i = 0; i < n; i++){
+        std::cin >> temps[i];
+        if (temps[i] < 0)
+            frz++;
     }
-    return days;
+    std::cout << frz << std::endl;
+    return 0;
 }
