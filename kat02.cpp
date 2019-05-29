9e5b91eba0cea7c1664bb1de344edb1482f8041f
Kaid <kaitliny12@gmail.com>
2019-05-28 20:02:58 -0400

    kat02

--- kat02.cpp
+++ kat02.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 
-bool stones(int n){
-    for (int i = 1; i < n - 1; i += 2){
-        for (int n; n >= 0; n -= 2){
-
-        }
-        if (n % 2 == 1)
-            return true;
-    }
-}
 int main(){
-    int n = 2;
-    stones(n);
-    if (stones)
-        std::cout<< "Alice" << std::endl;
+    int n;
+    std::cin >> n;
+    if (n % 2 == 0) 
+        std::cout << "Bob" << std::endl;
     else
-        std::cout<< "Bob" << std::endl;    
+        std::cout << "Alice" << std::endl; 
     return 0;
 }
